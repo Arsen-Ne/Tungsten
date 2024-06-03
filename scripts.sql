1e0d4fb99528b2642c70e6af5fd2e3ec1eabacf2
Arsenyn <arsen.nesytykh@gmail.com>
2024-06-03 18:35:37 +0300

    Update DB script: add bd name

--- scripts.sql
+++ scripts.sql
@@ -1,6 +1,10 @@
+Create database user_database;
+
+use user_database;
+
 CREATE TABLE User (
   user_id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
+  username VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL
 );
